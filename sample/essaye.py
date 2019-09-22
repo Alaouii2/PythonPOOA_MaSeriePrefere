93933e44f002c26b20e3abda3613fe45b13a5a50
Adrien <30908946+zizol@users.noreply.github.com>
2019-09-22 19:08:29 +0200

    Début des tests doc

---
+++ sample/essaye.py
@@ -0,0 +1,29 @@
+"""
+    Ce module est tellement inutile c'est incroyable...
+"""
+
+def fonctionbizarre(n):
+    """
+    En fait personne n'est sensé le savoir, mais cette fonction fait en réalité quelque chose de très bateau.
+    Elle renvoie juste l'argument qu'on lui envoie. Truc de fou non ?
+
+    :param arg1: description
+    :param arg2: description
+    :type arg1: type
+    :type arg2: type
+    :return: description de la valeur de retour
+    :rtype: type de la valeur de retour
+
+    :Example:
+
+    Un exemple écrit après un saut de ligne.
+
+    >>> fonctionbizarre("n")
+    "n"
+
+    .. seealso:: Référence à une autre partie du code
+    .. warning:: Avertissement
+    .. note:: Note
+
+    """
+    print(n)
