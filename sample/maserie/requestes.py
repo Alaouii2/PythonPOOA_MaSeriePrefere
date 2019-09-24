b687b3e1c1617079e18a5682d001ebc43afb670b
Adrien <30908946+zizol@users.noreply.github.com>
2019-09-24 16:00:10 +0200

    Requests

--- sample/maserie/requestes.py
+++ sample/maserie/requestes.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://api.betaseries.com/shows/list"
 
-querystring = {"key":"7c2f686dfaad","v":"3.0","limit":"10"}
+querystring = {"key":"7c2f686dfaad","v":"3.0","limit":"1"}
 
 response = requests.request("GET", url, params=querystring)
 
