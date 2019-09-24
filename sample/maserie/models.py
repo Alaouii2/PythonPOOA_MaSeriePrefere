12d2111a142916be769000d01bb06127270c2ab5
Zizol <adrien.videgrain@gmail.com>
2019-09-24 16:46:39 +0200

    DB Foiré presque possible de recréer la DB au lancement

--- sample/maserie/models.py
+++ sample/maserie/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-
 from .views import app
+import logging as lg
 
 # Create database connection object
 db = SQLAlchemy(app)
@@ -14,4 +14,10 @@ class Content(db.Model):
         self.description = description
         self.gender = gender
 
-db.create_all()
+def init_db():
+    db.drop_all()
+    db.create_all()
+    db.session.add(Content("This is SPARTAAAAA!!!", 1))
+    db.session.add(Content("What's your favorite scary movie ?", 0))
+    db.session.commit()
+    lg.warning('Database initialized !')
