3ad3308b8c9996e2ae14de537611df4d11d0428a
Adrien <30908946+zizol@users.noreply.github.com>
2019-10-01 14:10:09 +0200

    models non commenté

--- sample/maserie/models.py
+++ sample/maserie/models.py
@@ -11,52 +11,17 @@ from .views import app
 db = SQLAlchemy(app)
 
 class Content(db.Model):
-    """
-        Adds two numbers and returns the result.
-    """
+
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(200), nullable=False)
     gender = db.Column(db.Integer(), nullable=False)
 
     def __init__(self, description, gender):
-        """
-            Adds two numbers and returns the result.
-        """
         self.description = description
         self.gender = gender
 
-    def quelconque(self):
-        """
-        Test
-        :return: rien
-        """
-        pass
 
 def init_db():
-    """
-            Adds two numbers and returns the result.
-
-        This add two real numbers and return a real result. You will want to
-        use this function in any place you would usually use the ``+`` operator
-        but requires a functional equivalent.
-
-        :param a: The first number to add
-        :param b: The second number to add
-        :type a: int
-        :type b: int
-        :return: The result of the addition
-        :rtype: int
-
-        :Example:
-
-        >>> add(1, 1)
-        2
-        >>> add(2.1, 3.4)  # all int compatible types work
-        5.5
-
-        .. seealso:: sub(), div(), mul()
-        .. note:: You may want to use a lambda function instead of this.
-    """
     db.drop_all()
     db.create_all()
     db.session.add(Content("This is iughukygv!!!", 1))
