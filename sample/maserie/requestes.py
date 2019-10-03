a9cafc134dab12365b544c7338aa6ab5ff6e5158
Adrien <30908946+zizol@users.noreply.github.com>
2019-10-03 16:45:03 +0200

    Requetes API avancement

--- sample/maserie/requestes.py
+++ sample/maserie/requestes.py
@@ -3,7 +3,7 @@
 """
 
 import requests
-
+from models import db, Client, Serie_disponible
 
 # Page d'accueil
 def accueil():
@@ -21,11 +21,11 @@ def serie_par_genre():
     return result
 
 # tri des series par date
-def serie_par_date():
-    url = "https://api.betaseries.com/shows/list"
-    querystring = {"key": "7c2f686dfaad", "v": "3.0", "limit": "2", "recent":True}
-    result = requests.request("GET", url, params=querystring)
-    return result
+# def serie_par_date():
+#     url = "https://api.betaseries.com/shows/list"
+#     querystring = {"key": "7c2f686dfaad", "v": "3.0", "limit": "2", "recent":True}
+#     result = requests.request("GET", url, params=querystring)
+#     return result # Pas trivial
 
 # tri des series par ordre alphabetique
 # def serie_par_alphabet():
@@ -42,12 +42,22 @@ def fiche_serie(id):
     result = requests.request("GET", url, params=querystring)
     url2 = "https://api.betaseries.com/shows/seasons"
     result2 = requests.request("GET", url2, params=querystring)
-    return result, result2
+    url3 = "https://api.betaseries.com/shows/display"
+    result3 = requests.request("GET", url3, params=querystring)
+    return result, result2, result3
+
+# rechercher une serie par un nom
+def recherche_serie(title):
+    url = "https://api.betaseries.com/shows/search"
+    querystring = {"key": "7c2f686dfaad", "v": "3.0", "nbpp": "10", "title":title}
+    result = requests.request("GET", url, params=querystring)
+    return result
 
 
 # inscription a la base utilisateur
-def inscription_base():
-    pass
+def inscription_base(adresse, mdp, user):
+    db.session.add(Client(adresse, mdp, user))
+    db.session.commit()
 
 # connection a la base utilisateur
 def connection_base():
@@ -64,12 +74,4 @@ def ajout_préférée():
 # notification serie preferee et recente
 def notification():
     pass
-
-
-# rechercher une serie par un nom
-def recherche_serie(title):
-    url = "https://api.betaseries.com/shows/search"
-    querystring = {"key": "7c2f686dfaad", "v": "3.0", "nbpp": "10", "title":title}
-    result = requests.request("GET", url, params=querystring)
-    return result
 
